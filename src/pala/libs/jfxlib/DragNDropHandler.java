1bfaab471cb7e5ea4384ed28399947de3c604d91
Palanath <pala@dusttoash.org>
2023-07-22 12:45:10 -0500

    Create DragNDropHandler

---
+++ src/pala/libs/jfxlib/DragNDropHandler.java
@@ -0,0 +1,14 @@
+package pala.libs.jfxlib;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.DragEvent;
+
+public class DragNDropHandler implements EventHandler<DragEvent> {
+
+	@Override
+	public void handle(DragEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
