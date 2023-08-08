8dd9ed48c889ef1da924da53c48d010e9a51b578
Palanath <pala@dusttoash.org>
2023-08-08 18:20:21 -0500

    Update JavaFXUtils

--- src/pala/libs/jfxlib/JavaFXUtils.java
+++ src/pala/libs/jfxlib/JavaFXUtils.java
@@ -1,5 +1,14 @@
 package pala.libs.jfxlib;
 
+import java.util.function.Function;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -54,5 +63,63 @@ public class JavaFXUtils {
 	public static void setSize(Stage stage, double width, double height) {
 		stage.setWidth(width);
 		stage.setHeight(height);
+	}
+
+	/**
+	 * <p>
+	 * Returns an object binding that is represents the nested property, returned
+	 * from the provided <code>propertyAccessor</code>, that is contained within the
+	 * provided parent property. When this nested property changes value, the
+	 * returned object binding is updated, and when the parent property provided
+	 * changes value, the returned property is updated to reflect the new parent's
+	 * nested property's value.
+	 * </p>
+	 * <p>
+	 * This is similar to the {@link #property(ObservableValue, Function)} method,
+	 * but this method is for nested observables rather than nested constant values.
+	 * </p>
+	 * 
+	 * @param <P>              The type of the parent property's value.
+	 * @param <R>              The type of the child property's value.
+	 * @param parentProperty   The parent property. For example, an instance of
+	 *                         <code>ObjectProperty&lt;VBox&gt;</code>.
+	 * @param propertyAccessor A {@link Function} used to access the child property
+	 *                         within the parent property. Often of the form,
+	 *                         <code>ParentValueType::someFieldProperty</code>, for
+	 *                         example, <code>VBox::backgroundProperty</code>.
+	 * @return The created {@link ObjectBinding}.
+	 */
+	public static <P, R> NewObjectBinding<R> nested(ObservableValue<? extends P> parentProperty,
+			Function<? super P, ? extends ObservableValue<? extends R>> propertyAccessor) {
+		class O extends NewObjectBinding<R> implements ChangeListener<P> {
+			{
+				bind(parentProperty);
+				if (parentProperty.getValue() != null)
+					bind(propertyAccessor.apply(parentProperty.getValue()));
+			}
+
+			@Override
+			public void changed(ObservableValue<? extends P> observable, P oldValue, P newValue) {
+				if (oldValue != null)
+					unbind(propertyAccessor.apply(oldValue));
+				if (newValue != null)
+					bind(propertyAccessor.apply(newValue));
+			}
+
+			@Override
+			public void dispose() {
+				if (parentProperty.getValue() != null)
+					unbind(propertyAccessor.apply(parentProperty.getValue()));
+				unbind(parentProperty);
+			}
+
+			@Override
+			protected R computeValue() {
+				P p = parentProperty.getValue();
+				return p == null ? null : propertyAccessor.apply(p).getValue();
+			}
+
+		}
+		return new O();
 	}
 }
